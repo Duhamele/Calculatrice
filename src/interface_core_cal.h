f37b63f682471925bf0634b9700e97f0cfb9d3bd
duhamel <elie.duhamel@grenoble-inp.org>
2026-09-17 21:25:58 +0200

    Commentaire for interface core

--- src/interface_core_cal.h
+++ src/interface_core_cal.h
@@ -2,18 +2,33 @@
 #define INTERFACE_CORE_CAL_H
 #include <QObject>
 #include <QTextEdit>
+
+/**
+ * @brief The Interface_core_cal class
+ * Core logical calcul
+ */
 class Interface_core_cal:public QObject
 {
     Q_OBJECT
 public:
     Interface_core_cal(QTextEdit* output);
+    /**
+     * @brief addchar for equation
+     * @param symbol is number or /,*,-,+.
+     */
     void addchar(QChar symbol);
+    /**
+     * @brief remove the equation in writing
+     */
     void clear();
+    /**
+     * @brief equal start compute
+     */
     void equal();
 private:
     QString string_selc;
     QString string_old;
-     QTextEdit * const output;
+    QTextEdit * const output;
     void updateOutput();
 };
 
