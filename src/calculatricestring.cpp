7d5452d615184b6d8b9862cebd9a64f6b8d8949f
duhamel <elie.duhamel@grenoble-inp.org>
2026-09-17 21:27:46 +0200

    impl : parser for equation

---
+++ src/calculatricestring.cpp
@@ -0,0 +1,74 @@
+#include "calculatricestring.h"
+#include <sys/types.h>
+#include <QString>
+CalculatriceString::CalculatriceString() {}
+
+
+
+enum class TypeTokenComputeString: u_int8_t{
+    Number,
+    Add,
+    Sub,
+    Mul,
+    Div,
+};
+
+struct TokenComputeString
+{
+    TypeTokenComputeString type;
+    union {
+        int number;
+    };
+
+};
+
+
+TokenComputeString QStringNumberToTokenComputeString(QString string){
+    int val=string.toInt();
+    return {.type=TypeTokenComputeString::Number,.number=val};
+};
+TokenComputeString QCharOpToTokenComputeString(QChar op){
+    switch (op.toLatin1()) {
+    case '/':
+        return {TypeTokenComputeString::Div};
+        break;
+    case '+':
+        return {.type=TypeTokenComputeString::Add};
+        break;
+    case '*':
+        return {.type=TypeTokenComputeString::Mul};
+        break;
+    case '-':
+        return {.type=TypeTokenComputeString::Add};
+    default:
+        throw new std::invalid_argument("");
+        break;
+    };
+}
+
+std::vector<TokenComputeString> TokenizerComputeString(QString string){
+    QString number="";
+    std::vector<TokenComputeString> array;
+    for( int i=0; i<string.size();i++){
+        if(string.at(i).isDigit()){
+            number.append(string.at(i));
+        }else{
+            if(!number.isEmpty()){
+                array.push_back(QStringNumberToTokenComputeString(number));
+                number.clear();
+            }
+            array.push_back(QCharOpToTokenComputeString(string.at(i)));
+
+        }
+    }
+    if(!number.isEmpty()){
+        array.push_back(QStringNumberToTokenComputeString(number));
+        number.clear();
+    }
+    return array;
+
+}
+
+
+
+
---
+++ src/calculatricestring.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATRICESTRING_H
+#define CALCULATRICESTRING_H
+
+class CalculatriceString
+{
+public:
+    CalculatriceString();
+};
+
+#endif // CALCULATRICESTRING_H
