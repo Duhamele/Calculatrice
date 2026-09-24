9558f94356e2663b12b8639700976b621d8f46bf
duhamel <elie.duhamel@grenoble-inp.org>
2026-09-24 21:54:13 +0200

    feat: equal NumberDynamie

--- src/cal_math.h
+++ src/cal_math.h
@@ -1,7 +1,6 @@
 #ifndef MATH_H
 #define MATH_H
 #include <sys/types.h>
-
 enum class TypeNumber: u_int8_t{
     Float,
     Int,
@@ -59,6 +58,14 @@ public:
             val2=isInt(other.type)?static_cast<float>(other.Int):other.Float;
         return NumberDynamy(val1/val2);
     };
+    bool operator==(NumberDynamy other){
+        if(isInt(this->type)&&isInt(other.type)){
+            return this->Int==other.Int;
+        }
+        float val1=isInt(this->type)?static_cast<float>(this->Int):this->Float,
+            val2=isInt(other.type)?static_cast<float>(other.Int):other.Float;
+        return val1==val2;
+    }
 
 };
 
